55502ffaa788441de7bc410a7407326a1c136964
Jang Junha <junha113@daum.net>
2015-11-11 17:51:25 +0900

    a9 edit

--- static/index.js
+++ static/index.js
@@ -1,3 +1,24 @@
+function calc(dms) {
+	var ms = dms;
+        var sec = ms / 1000;    ms %= 1000;
+        var min = sec / 60;             sec %= 60;
+        var hr = min / 60;              min %= 60;
+        var day = hr / 24;              hr %= 24;
+        ms = Math.floor(ms);
+        sec = Math.floor(sec);
+        min = Math.floor(min);
+        hr = Math.floor(hr);
+        day = Math.floor(day);
+
+        var blue = Math.floor(dms / (365 * 24 * 60 * 60 * 1000) * 255);
+        var red = 255 - blue;
+        var color = 'rgb(' + red + ',' + blue + ',0)';
+
+        var res = day + "일 " + hr + "시간 " + min + "분 " + sec + "초 " + ms;
+	return {'color': color, 'text': res};
+}
+
+
 $(document).ready(function() {
 	$('#message').keypress(function(e) {
 		if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
@@ -18,42 +39,20 @@ $(document).ready(function() {
 		if (dms < 0) {
 			if (then_a9.getTime() - now.getTime() >= 0) {
 				// A9 SPECIAL PAGE
-				then = then_a9;
-				dms = then.getTime() - now.getTime();
-				
 				if (!a9) {
 					a9 = true;
-					$('h1').text('앱-잼');
-					$('body').css('cursor', 'pointer');
-					$('body').click(function() { location.href="//www.appjam.kr"; });
-					$('#message').click(function() {
-						if (!event) window.event.cancelBubble(true);
-						else event.stopPropagation();
-					});
+					$('#box').click(function() { location.href="//www.appjam.kr"; });
+                    $('#box').css('display', 'block');
 				}
-			} else {
-				then = then_next;
-				dms = then.getTime() - now.getTime();
+				$('#box-text').text(calc(then_a9.getTime() - now.getTime()).text);
 			}
+            then = then_next;
+            dms = then.getTime() - now.getTime();
 		}
 
-		var ms = dms;
-		var sec = ms / 1000;	ms %= 1000;
-		var min = sec / 60;		sec %= 60;
-		var hr = min / 60;		min %= 60;
-		var day = hr / 24;		hr %= 24;
-		ms = Math.floor(ms);
-		sec = Math.floor(sec);
-		min = Math.floor(min);
-		hr = Math.floor(hr);
-		day = Math.floor(day);
+		var res = calc(dms);		
 
-		var blue = Math.floor(dms / (365 * 24 * 60 * 60 * 1000) * 255);
-		var red = 255 - blue;
-		var color = 'rgb(' + red + ',' + blue + ',0)';
-
-		var res = day + "일 " + hr + "시간 " + min + "분 " + sec + "초 " + ms;
-		$('#time').text(res);
-		$('#time').css('color', color);
+		$('#time').text(res.text);
+		$('#time').css('color', res.color);
 	},25);
 });
