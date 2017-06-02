3336334e871f75f3b36a47e490a5e38dcd53a6d9
jangjunha <jangjunha113@gmail.com>
2017-06-02 13:19:51 +0900

    Optimize site

--- static/index.js
+++ static/index.js
@@ -1,46 +1,36 @@
 function calc(dms) {
-	var ms = dms;
-        var sec = ms / 1000;    ms %= 1000;
-        var min = sec / 60;             sec %= 60;
-        var hr = min / 60;              min %= 60;
-        var day = hr / 24;              hr %= 24;
-        ms = Math.floor(ms);
-        sec = Math.floor(sec);
-        min = Math.floor(min);
-        hr = Math.floor(hr);
-        day = Math.floor(day);
+  var ms = dms;
+  var sec = ms / 1000;  ms %= 1000;
+  var min = sec / 60;       sec %= 60;
+  var hr = min / 60;        min %= 60;
+  var day = hr / 24;        hr %= 24;
+  ms = Math.floor(ms);
+  sec = Math.floor(sec);
+  min = Math.floor(min);
+  hr = Math.floor(hr);
+  day = Math.floor(day);
 
-        var blue = Math.floor(dms / (365 * 24 * 60 * 60 * 1000) * 255);
-        var red = 255 - blue;
-        var color = 'rgb(' + red + ',' + blue + ',0)';
+  var blue = Math.floor(dms / (365 * 24 * 60 * 60 * 1000) * 255);
+  var red = 255 - blue;
+  var color = 'rgb(' + red + ',' + blue + ',0)';
 
-        var res = day + "일 " + hr + "시간 " + min + "분 " + sec + "초 " + ms;
-	return {'color': color, 'text': res};
+  var res = day + "일 " + hr + "시간 " + min + "분 " + sec + "초 " + ms;
+  return {'color': color, 'text': res};
 }
 
+setInterval(function() {
+  var now = new Date();
+  then = new Date(2016, 11 - 1, 17, 8, 40, 0, 0);
+  then_next = new Date(2017, 11 - 1, 16, 8, 40, 0, 0);
 
-$(document).ready(function() {
-	$('#message').keypress(function(e) {
-		if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-			submit();
-			return false;
-		}
-		return true;
-	});
+  var dms = then.getTime() - now.getTime();
+  if (dms < 0) {
+    dms = then_next.getTime() - now.getTime();
+  }
 
-	setInterval(function() {
-		var now = new Date();
-		then = new Date(2016, 11 - 1, 17, 8, 40, 0, 0);
-		then_next = new Date(2017, 11 - 1, 16, 8, 40, 0, 0);
+  var res = calc(dms);  
 
-		var dms = then.getTime() - now.getTime();
-		if (dms < 0) {
-			dms = then_next.getTime() - now.getTime();
-		}
-
-		var res = calc(dms);		
-
-		$('#time').text(res.text);
-		$('#time').css('color', res.color);
-	},25);
-});
+  var timeElem = document.querySelector('#time');
+  timeElem.innerHTML = res.text;
+  timeElem.style.color = res.color;
+},25);
